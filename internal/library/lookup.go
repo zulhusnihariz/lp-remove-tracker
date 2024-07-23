5021552766d30b185b9330c6d5499a4909fcab0a
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2024-07-23 20:02:00 +0800

    fix lookup table storage

--- internal/library/lookup.go
+++ internal/library/lookup.go
@@ -16,8 +16,9 @@ type LookupIndex struct {
 	LookupTableKey   string
 }
 
-func GetLookupTable(addr solana.PublicKey) (addresslookuptable.AddressLookupTableState, error) {
-	redisClient, err := adapter.GetRedisClient(4)
+// TODO: Lookup table is not store in redis!!!
+func GetLookupTable(addr solana.PublicKey) (*addresslookuptable.AddressLookupTableState, error) {
+	redisClient, err := adapter.GetRedisClient(3)
 	if err != nil {
 		log.Fatalf("Failed to get LookupTableStorage instance: %v", err)
 	}
@@ -25,18 +26,36 @@ func GetLookupTable(addr solana.PublicKey) (addresslookuptable.AddressLookupTabl
 	account, err := storage.GetLookup(redisClient, addr.String())
 
 	if err != nil {
-		return account, err
+		if err.Error() != "key not found" {
+			return &addresslookuptable.AddressLookupTableState{}, err
+		}
+	}
+
+	if account.Authority != nil {
+		return &account, nil
 	}
 
 	resp, err := rpc.GetLookupTable(addr)
 
 	if err != nil {
-		return addresslookuptable.AddressLookupTableState{}, err
+		return &addresslookuptable.AddressLookupTableState{}, err
 	}
 
 	storage.SetLookup(redisClient, addr.String(), resp)
 
-	return resp, nil
+	return &resp, nil
+}
+
+func SetLookupTable(ammId solana.PublicKey, lookup *addresslookuptable.AddressLookupTableState) error {
+	redisClient, err := adapter.GetRedisClient(3)
+	if err != nil {
+		log.Fatalf("Failed to get LookupTableStorage instance: %v", err)
+		return err
+	}
+
+	err = storage.SetLookup(redisClient, ammId.String(), *lookup)
+
+	return nil
 }
 
 func GenerateTableLookup(addressTableLookups []generators.TxAddressTableLookup) []LookupIndex {
